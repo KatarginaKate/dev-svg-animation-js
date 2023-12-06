6d4796d32880226ce4f8afb438cfd25960fc35f9
KatarginaKate <katrin-kate@ukr.net>
2023-12-06 17:12:32 +0200

    active?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,33 @@
-const animationPath = document.querySelectorAll('.animation-path');
-const buttons = document.querySelectorAll('.btn');
+const installAnimatedPanel = () => {
+  const animationPath = document.querySelectorAll('.animation-path');
+  const buttons = document.querySelectorAll(".btn");
+  const animated = document.querySelector(".animated");
+  
 
-function removeActiveClass() {
-    buttons.forEach(item => {
-        item.classList.remove('active')
+  function removeActiveClass() {
+    buttons.forEach((item) => {
+      item.classList.remove("active")
     })
-} 
+  }
+
+  const changeCase = (event) => {
+   
+    removeActiveClass();
+    event.target.classlist.add ('active')
+    const getBtnCase = event.target.getAttribute("data-animation")
+    const case3 = document.querySelector('#' + getBtnCase)
+    case3.classlist.add ('active')
+    
+  }
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", changeCase);
+  });
+
+  // animationPath.forEach(path => {
+  //     console.log(path);
+  // })
+}
+ document.querySelector(".panel") ? installAnimatedPanel() : null;
 
 
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        removeActiveClass();
-        btn.classList.add('active')
-    })
-})
-
-animationPath.forEach(path => {
-    console.log(path);
-})
